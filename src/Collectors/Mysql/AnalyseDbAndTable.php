8bc1b4df93e6d1f9bde3221472c7dde527435e59
Nikolas <nikolas.tatianenko@gmail.com>
2016-04-25 00:39:41 +0300

    Get Columns method

--- src/Collectors/Mysql/AnalyseDbAndTable.php
+++ src/Collectors/Mysql/AnalyseDbAndTable.php
@@ -32,4 +32,19 @@ class AnalyseDbAndTable extends Basic {
 
     return $data;
   }
+
+  function getInfoColumns($database = '', $table_name) {
+    if (!$database) {
+      $database = $this->database;
+    }
+    $query = "SELECT * FROM information_schema.columns WHERE table_schema = '$database' and table_name ='$table_name'";
+
+    $result = mysql_query($query);
+
+    while ($row = mysql_fetch_assoc($result)) {
+      $data[] = $row;
+    }
+
+    return $data;
+  }
 }
