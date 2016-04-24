d397b6dcbed2cb1dc65547184ebb5782505a1ba3
Nikolas <nikolas.tatianenko@gmail.com>
2016-04-25 00:39:11 +0300

    Created GetInfo method

--- src/Collectors/Mysql/AnalyseDbAndTable.php
+++ src/Collectors/Mysql/AnalyseDbAndTable.php
@@ -7,6 +7,29 @@
 namespace src\Collectors\Mysql;
 
 
-class AnalyseDbAndTable extends Base {
+class AnalyseDbAndTable extends Basic {
 
+  /**
+   * Analyse table.
+   *
+   * @param string $database
+   *   Database.
+   *
+   * @return array
+   *   Array with sql variabels.
+   */
+  function getInfo($database = '') {
+    if (!$database) {
+      $database = $this->database;
+    }
+    $query = "SELECT * FROM information_schema.tables WHERE table_schema = '$database'";
+
+    $result = mysql_query($query);
+
+    while ($row = mysql_fetch_assoc($result)) {
+      $data[] = $row;
+    }
+
+    return $data;
+  }
 }
