b638b0379dd49be8dad19748cbdc7ce5065c0971
Nikolas <nikolas.tatianenko@gmail.com>
2016-04-24 23:13:27 +0300

    Created Constructor and destructor

--- src/Collectors/Mysql/Basic.php
+++ src/Collectors/Mysql/Basic.php
@@ -8,5 +8,16 @@ namespace src\Collectors\Mysql;
 
 
 abstract class Basic {
+  protected $link = NULL;
 
+  function __construct($username, $password, $dbname = NULL, $host = 'localhost') {
+    $this->link = mysql_connect($host, $username, $password);
+    if (!$this->link) {
+      die('Fail: ' . mysql_error());
+    }
+  }
+
+  function __destruct() {
+    mysql_close($this->link);
+  }
 }
