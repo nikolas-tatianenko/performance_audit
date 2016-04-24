442343dfecfc344ac861ca98f8738a4e434e05ba
Nikolas <nikolas.tatianenko@gmail.com>
2016-04-24 23:18:32 +0300

    Added method 'GetInfo'.

--- src/Collectors/Mysql/GetMysqlInfo.php
+++ src/Collectors/Mysql/GetMysqlInfo.php
@@ -8,4 +8,18 @@ namespace src\Collectors\Mysql;
 
 class GetMysqlInfo extends Basic {
 
+  /**
+   * Get sql data.
+   *
+   * @return array
+   *   Array with sql variabels.
+   */
+  function getInfo() {
+    $result = mysql_query('SHOW VARIABLES');
+    while ($row = mysql_fetch_assoc($result)) {
+      $data[$row['Variable_name']] = $row['Value'];
+    }
+
+    return $data;
+  }
 }
