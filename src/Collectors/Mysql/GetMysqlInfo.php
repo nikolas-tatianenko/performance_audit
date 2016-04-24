6151397c9e271b693ffea02387dea60a7a7f4d39
Nikolas <nikolas.tatianenko@gmail.com>
2016-04-24 23:29:42 +0300

    Check specific variable in MySql

--- src/Collectors/Mysql/GetMysqlInfo.php
+++ src/Collectors/Mysql/GetMysqlInfo.php
@@ -11,11 +11,18 @@ class GetMysqlInfo extends Basic {
   /**
    * Get sql data.
    *
+   * @param string $variable
+   *   Variable for query.
+   *
    * @return array
    *   Array with sql variabels.
    */
-  function getInfo() {
-    $result = mysql_query('SHOW VARIABLES');
+  function getInfo($variable = '') {
+    $query = 'SHOW VARIABLES';
+    if ($variable) {
+      $query .= " LIKE '%$variable%'";
+    }
+    $result = mysql_query($query);
     while ($row = mysql_fetch_assoc($result)) {
       $data[$row['Variable_name']] = $row['Value'];
     }
